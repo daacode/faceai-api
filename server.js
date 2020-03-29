dd28fdeab0b2b166fe6a09ffb007e7d9fec80caa
Daagital <adeneyeabiodun@gmail.com>
2020-03-29 22:14:33 +0100

    signin route

--- server.js
+++ server.js
@@ -1,13 +1,56 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "john",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: "124",
+      name: "sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+};
+
 app.get("/", (req, res) => {
-  res.send("this is working");
+  res.send(database.users);
 });
 
 app.post("/signin", (req, res) => {
-  res.json("signing");
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
+
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "123",
+    name: name,
+    email: email,
+    password: password,
+    enteries: 0,
+    joined: new Date()
+  });
+  res.json(database.users[database.users.length - 1]);
 });
 
 app.listen(3000, () => {
