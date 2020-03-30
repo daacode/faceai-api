c0460864e76941ca5b0540ece12b9a33f589e58a
Daagital <adeneyeabiodun@gmail.com>
2020-03-30 21:32:58 +0100

    profile route

--- server.js
+++ server.js
@@ -53,6 +53,31 @@ app.post("/register", (req, res) => {
   res.json(database.users[database.users.length - 1]);
 });
 
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
+app.post("/image", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+});
+
 app.listen(3000, () => {
   console.log("app is running on port 3000");
 });
