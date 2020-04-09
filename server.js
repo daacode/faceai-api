69e182e55a6113322d9d176be8e2a746788adf46
Daagital <adeneyeabiodun@gmail.com>
2020-04-09 22:10:41 +0100

    signin connected to the database

--- server.js
+++ server.js
@@ -54,9 +54,25 @@ app.get("/", (req, res) => {
 app.post("/signin", (req, res) => {
   db.select("email", "hash")
     .from("login")
+    .where("email", "=", req.body.email)
     .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
       console.log(data);
-    });
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then((user) => {
+            console.log(user);
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json("unable to get user"));
+      } else {
+        res.status(400).json("wrong credentials");
+      }
+    })
+    .catch((err) => res.status(400).json("wrong credential"));
 });
 
 app.post("/register", (req, res) => {
