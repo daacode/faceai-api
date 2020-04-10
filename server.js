9e26749bc0ca0040816b82dbf9d553046c4d2a93
Daagital <adeneyeabiodun@gmail.com>
2020-04-10 21:34:14 +0100

    change made in localhost image

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get("/profile/:id", (req, res) => {
 app.put("/image", (req, res) => {
   const { id } = req.body;
   db("users")
-    .where("id", "", id)
+    .where("id", "=", id)
     .increment("entries", 1)
     .returning("entries")
     .then((entries) => {
