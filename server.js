2cc742e25dff18031da2b3fccbd0a223be0fdaba
Daagital <adeneyeabiodun@gmail.com>
2020-04-12 02:14:33 +0100

    signin controller created

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const knex = require("knex");
 
 const register = require("./controllers/register");
+const signin = require("./controllers/signin");
 
 const db = knex({
   client: "pg",
@@ -26,25 +27,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  db.select("email", "hash")
-    .from("login")
-    .where("email", "=", req.body.email)
-    .then((data) => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db
-          .select("*")
-          .from("users")
-          .where("email", "=", req.body.email)
-          .then((user) => {
-            res.json(user[0]);
-          })
-          .catch((err) => res.status(400).json("unable to get user"));
-      } else {
-        res.status(400).json("wrong credentials");
-      }
-    })
-    .catch((err) => res.status(400).json("wrong credential"));
+  signin.handleSignin(req, res, db, bcrypt);
 });
 
 app.post("/register", (req, res) => {
