b48f68fce256b36507d63f01e8cf8c27dbe53d4e
Daagital <adeneyeabiodun@gmail.com>
2020-04-13 18:43:41 +0100

    code modfied

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require("clarifai");
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+  apiKey: "b3aef77b71f6459194e7c52bb7abd863",
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("unable to work with API"));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db("users")
@@ -12,4 +28,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage,
+  handleApiCall,
 };
--- server.js
+++ server.js
@@ -37,6 +37,9 @@ app.get("/profile/:id", (req, res) => {
 app.put("/image", (req, res) => {
   image.handleImage(req, res, db);
 });
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 /* 
 bcrypt.hash("bacon", null, null, function(err, hash) {
